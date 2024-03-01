c48410af4fe3d98c325ca385714c922e55c9fb3d
Eduardo Ramírez <53280540+eduardordev@users.noreply.github.com>
2024-03-01 10:32:22 -0600

    cambios

--- back/facturacionapp/productFaker.py
+++ back/facturacionapp/productFaker.py
@@ -7,14 +7,18 @@ fake = Faker()
 productList = ['Ventilador', 'Tarjeta Grafica', 'Mouse', 'Monitor', 'Case', 'Pasta Termica', 'Laptop', 'Procesador', 'Audifonos', 'Power Bank']
 categories = ['Computo', 'Gaming', 'Estudio', 'Electronicos']
 
-def generate_fake_product():
-    product = {
-        'name': fake.random_element(productList),
-        'description': fake.sentence(),
-        'price': round(random.uniform(10.0, 1000.0), 2),
-        'category': random.choice(categories),
-    }
-    return product
+def generate_fake_product(used_products):
+    while True:
+        product = {
+            'producto': fake.random_element(productList),
+            'category': random.choice(categories),
+            'descripcion': fake.sentence(),
+            'quantity': random.randint(1, 40),
+            'price': round(random.uniform(10.0, 1000.0), 2),
+        }
+        if product['producto'] not in used_products:
+            used_products.add(product['producto'])
+            return product
 
 def save_products_to_file(products, output_file='products.json'):
     with open(output_file, 'w') as json_file:
@@ -22,9 +26,10 @@ def save_products_to_file(products, output_file='products.json'):
 
 # Ejemplo de uso
 if __name__ == "__main__":
-    num_products = 300
+    num_products = 10
+    used_products = set()
 
-    generated_products = [generate_fake_product() for _ in range(num_products)]
+    generated_products = [generate_fake_product(used_products) for _ in range(num_products)]
 
     for i, fake_product in enumerate(generated_products, start=1):
         print(f"Producto {i}: {fake_product}")
