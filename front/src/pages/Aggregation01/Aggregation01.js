883f3ef7aeafd38377fb96cda04a634340c4255d
Alejandro Ortega <ort18248@uvg.edu.gt>
2024-03-02 22:17:17 -0600

    Título y fallback por si no hay datos para un año

--- front/src/pages/Aggregation01/Aggregation01.js
+++ front/src/pages/Aggregation01/Aggregation01.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import DataTable from '../../components/DataTable';
+import { Typography } from '@mui/material';
 
 function Aggregation01() {
     const [data, setData] = useState([]);
@@ -29,26 +30,35 @@ function Aggregation01() {
 
     return (
         <div>
+            <Typography variant="h4" gutterBottom>
+                Ventas mensuales por año
+            </Typography>
             <div>
                 <label htmlFor="year">Select Year:</label>
                 <select id="year" value={selectedYear} onChange={handleYearChange} style={{ fontSize: '16px', marginLeft: '10px', padding: '2px' }}>
+                    <option value="2022">2022</option>
                     <option value="2023">2023</option>
                     <option value="2024">2024</option>
                     {/* Add more years as needed */}
                 </select>
             </div>
-            <DataTable
-                table={{
-                    columns: [
-                        { Header: "year", accessor: "year", width: "25%" },
-                        { Header: "month", accessor: "month", width: "30%" },
-                        { Header: "sales", accessor: "sales" },
-                    ],
-                    rows: data
-                }}
-            />
+            {data.length > 0 ? (
+                <DataTable
+                    table={{
+                        columns: [
+                            { Header: "year", accessor: "year", width: "25%" },
+                            { Header: "month", accessor: "month", width: "30%" },
+                            { Header: "sales", accessor: "sales" },
+                        ],
+                        rows: data
+                    }}
+                />
+            ) : (
+                <Typography variant="body1">No data available for the selected year.</Typography>
+            )}
         </div>
     );
+    
 }
 
 export default Aggregation01;
